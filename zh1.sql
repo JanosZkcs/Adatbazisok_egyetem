0365fa232a3956ec3c354e552b04dbfddc3409d2
JanosZkcs <j.zakocs03@gmail.com>
2024-03-06 14:21:26 +0100

    3.feladat megoldása zh1

--- zh1.sql
+++ zh1.sql
@@ -6,9 +6,16 @@ FROM FOGLALAS
 WHERE UGYFEL_FK LIKE 'emese'*/
 
 --2.feladat
-SELECT  szh.SZALLAS_ID, 
+/*SELECT  szh.SZALLAS_ID, 
         szh.SZALLAS_NEV,
         COUNT(sz.KLIMAS) AS 'Klímás szobák'
 FROM Szallashely szh JOIN Szoba sz ON szh.SZALLAS_ID = sz.SZALLAS_FK
 WHERE sz.KLIMAS = 'i'
-GROUP BY szh.SZALLAS_ID, szh.SZALLAS_NEV
+GROUP BY szh.SZALLAS_ID, szh.SZALLAS_NEV*/
+
+--3.feladat
+SELECT  szh.SZALLAS_NEV,
+        COUNT(f.FOGLALAS_PK) AS 'Foglalások száma'
+FROM FOGLALAS f RIGHT JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
+                JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
+GROUP BY szh.SZALLAS_NEV
