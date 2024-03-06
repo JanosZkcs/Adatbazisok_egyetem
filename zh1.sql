30eca11f79a5a6be85b7c90bbb42c559fdc86068
JanosZkcs <j.zakocs03@gmail.com>
2024-03-06 14:11:07 +0100

    zh1 - második feladat

--- zh1.sql
+++ zh1.sql
@@ -1,6 +1,14 @@
 --1.feladat
-SELECT  FOGLALAS_PK,
+/*SELECT  FOGLALAS_PK,
         METTOL,
         DATEADD(MONTH,1,METTOL) AS 'Módosított kezdődátum'
 FROM FOGLALAS
-WHERE UGYFEL_FK LIKE 'emese'
+WHERE UGYFEL_FK LIKE 'emese'*/
+
+--2.feladat
+SELECT  szh.SZALLAS_ID, 
+        szh.SZALLAS_NEV,
+        COUNT(sz.KLIMAS) AS 'Klímás szobák'
+FROM Szallashely szh JOIN Szoba sz ON sz.SZALLAS_FK = szh.SZALLAS_ID
+WHERE sz.KLIMAS = 'i'
+GROUP BY szh.SZALLAS_ID, szh.SZALLAS_NEV
