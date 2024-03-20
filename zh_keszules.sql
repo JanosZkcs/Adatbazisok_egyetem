bcafed997b90bdaa8529ab66e7fbe7cdb0439f1e
JanosZkcs <j.zakocs03@gmail.com>
2024-03-20 14:52:59 +0100

    1.feladat

--- zh_keszules.sql
+++ zh_keszules.sql
@@ -1,6 +1,6 @@
-SELECT  v.USERNEV,
+SELECT  IIF(v.USERNEV IS NULL,'Végösszeg',v.USERNEV),
         COUNT(DISTINCT szh.szallas_id)
 FROM Foglalas f LEFT JOIN Vendeg v ON f.ugyfel_fk = v.USERNEV
                 LEFT JOIN Szoba sz  on f.szoba_fk = sz.szoba_id
                 LEFT JOIN szallashely szh ON sz.szallas_fk = szh.szallas_id
-GROUP BY v.USERNEV
+GROUP BY ROLLUP(v.USERNEV)
