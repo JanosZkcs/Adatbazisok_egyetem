b7e25f1e8f3323525efebe5f1e0a1aebd66df31d
JanosZkcs <j.zakocs03@gmail.com>
2024-03-20 14:50:38 +0100

    1.gyakorlat

--- zh_keszules.sql
+++ zh_keszules.sql
@@ -1,6 +1,6 @@
 SELECT  v.USERNEV,
         COUNT(DISTINCT szh.szallas_id)
 FROM Foglalas f LEFT JOIN Vendeg v ON f.ugyfel_fk = v.USERNEV
-                JOIN Szoba sz  on f.szoba_fk = sz.szoba_id
-                JOIN szallashely szh ON sz.szallas_fk = szh.szallas_id
+                LEFT JOIN Szoba sz  on f.szoba_fk = sz.szoba_id
+                LEFT JOIN szallashely szh ON sz.szallas_fk = szh.szallas_id
 GROUP BY v.USERNEV
